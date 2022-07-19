c22ff706ae5cb378ac1aa714c39b8a34bf231c39
robdll <dilillo.roberto@gmail.com>
2022-07-19 19:32:50 -0300

    create seo comp

---
+++ components/Seo.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Seo() {
+  return (
+    <Head>
+      <title>neobrutalism courseshop</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="neobrutalism design" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
