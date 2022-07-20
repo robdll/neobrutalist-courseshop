abbb74c1425facfc696f2238e2eba1dc96183fa2
robdll <dilillo.roberto@gmail.com>
2022-07-19 21:11:34 -0300

    improve roundbtn

--- components/RoundBtn.js
+++ components/RoundBtn.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "../styles/RoundBtn.module.scss";
 
-export default function RoundBtn({ icon }) {
+export default function RoundBtn({ icon, bordered }) {
+  const className = `${styles.circle} ${bordered ? styles.bordered : ""}`;
   return (
-    <span className={styles.circle}>
+    <button className={className}>
       <Image src={icon} alt="avatar" width="12" height="14  " />
-    </span>
+    </button>
   );
 }
