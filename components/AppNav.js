73d52a51b2ea2cb59c67a6f9e177ee5588dc3671
robdll <dilillo.roberto@gmail.com>
2022-07-19 21:11:18 -0300

    fix copy

--- components/AppNav.js
+++ components/AppNav.js
@@ -9,7 +9,7 @@ export default function AppNav({ name = "Rob.dll" }) {
       <span className={styles.avatar}>
         <Image src="/avatar.png" alt="avatar" width="50" height="50" />
       </span>
-      <span className={styles.welcome}>Hello {`${name}`}</span>
+      <span className={styles.welcome}>Hello {`${name}`},</span>
       <span className={styles.hint}>Get a new course today!</span>
       <RoundBtn icon="/bell.svg" />
     </nav>
